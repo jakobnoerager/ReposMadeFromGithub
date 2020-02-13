83333ff11909fd5dbdbe34749e0cc03472bb6ed4
Jakob Nørager Christensen <JNCH@easj.dk>
2020-02-13 13:05:59 +0100

    Initial Commit

---
+++ PushToGit/PushToGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PushToGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
