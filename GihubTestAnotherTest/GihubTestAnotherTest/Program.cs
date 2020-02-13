e10cda3c5e03f55c34e67b119e220af693789b63
Jakob Nørager Christensen <JNCH@easj.dk>
2020-02-13 12:58:00 +0100

    Changes the console text in main

---
+++ GihubTestAnotherTest/GihubTestAnotherTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GihubTestAnotherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! With changes");
+        }
+    }
+}
